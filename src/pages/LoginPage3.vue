f76b51a4a4726f0da5bae56c310a8cb932f280de
zabihullah <zabihullah58@gmail.com>
2025-09-20 00:49:08 -0700

    arrange them

--- src/pages/LoginPage3.vue
+++ src/pages/LoginPage3.vue
@@ -1,31 +1,35 @@
 <template>
-  <q-page class="flex items-start w-full  bg-[#FFF9F1]">
+  <q-page class="flex items-start w-full bg-[#FFF9F1]">
     <div class="flex justify-center items-center w-full pt-8">
-      <q-img src="../assets/Vector.png" class="w-[100px] h-[85px] " fit="contain" />
-</div>
+      <q-img
+        src="../assets/Vector.png"
+        class="w-[100px] h-[85px]"
+        fit="contain"
+      />
+    </div>
     <div class="grid grid-cols-12 px-4 w-full pb-32">
       <div class="col-span-12 md:col-span-8 md:col-start-3">
-        <q-card flat   class="rounded-2xl pb-20 flex flex-col items-center bg-transparent">
-
-           <q-card-section class=" max-w-[975px] px-[6px]  ">
-            <div class="flex items-center justify-center ">
-               <div
-              class="text-[#003329] text-center font-[Arena_Uno] text-[80px] font-normal max-md:text-[50px] "
-            >
-              Welcome Back
+        <q-card
+          flat
+          class="rounded-2xl pb-20 flex flex-col items-center bg-transparent"
+        >
+          <q-card-section class="max-w-[975px] px-[6px]">
+            <div class="flex items-center justify-center">
+              <div
+                class="text-[#003329] text-center font-[Arena_Uno] text-[80px] font-normal max-md:text-[50px]"
+              >
+                Welcome Back
+              </div>
+              <div
+                class="text-[#003329] text-center font-[Arena_Uno] text-[80px] font-extrabold"
+              >
+                Mr. Naseem Abdullah
+              </div>
             </div>
-            <div
-              class="text-[#003329] text-center font-[Arena_Uno] text-[80px] font-extrabold  "
-            >
-              Mr. Naseem Abdullah
-          </div>
-            </div>
-
-
           </q-card-section>
           <!-- Inputs -->
 
-          <CustomeSubmit text="Enter" @submit="handleLogin"  />
+          <CustomeSubmit text="Enter" @submit="handleLogin" />
         </q-card>
       </div>
     </div>
@@ -33,15 +37,5 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-import CustomInput from "../components/CustomeInput.vue";
 import CustomeSubmit from "../components/CustomeSubmit.vue";
-
-
-const email = ref("");
-
-
-
 </script>
-
-
--- src/pages/Newpassword.vue
+++ src/pages/Newpassword.vue
@@ -1,11 +1,11 @@
 <template>
-  <q-page class="flex items-start w-full bg-[#FFF9F1] ">
+  <q-page class="flex items-start w-full bg-[#FFF9F1]">
     <div class="flex justify-center items-center w-full py-8">
-        <q-img
-            src="../assets/Vector.png"
-            class="w-[100px] h-[85px]"
-            fit="contain"
-          />
+      <q-img
+        src="../assets/Vector.png"
+        class="w-[100px] h-[85px]"
+        fit="contain"
+      />
     </div>
     <div class="grid grid-cols-12 px-4 w-full pb-32">
       <q-form class="col-span-12 md:col-span-8 md:col-start-3">
@@ -15,30 +15,27 @@
         >
           <!-- Logo -->
 
-
           <!-- Title -->
-          <q-card-section class="flex flex-col justify-between ">
+          <q-card-section class="flex flex-col justify-between">
             <div
               class="text-[#003329] text-center font-[Arena_Uno] text-[37.311px] font-normal"
-
             >
               Please enter your new password
             </div>
-
           </q-card-section>
 
           <!-- Inputs -->
           <q-card-section
-            class="flex flex-col w-full max-w-[850px] gap-2 justify-center items-center "
+            class="flex flex-col w-full max-w-[850px] gap-2 justify-center items-center"
           >
             <div
-
-              class="flex flex-col items-start justify-center gap-2 w-full px-2  "
-              >
-
+              class="flex flex-col items-start justify-center gap-2 w-full px-2"
+            >
               <span class="perfectText">perfect</span>
-              <div class="  w-full h-[9px] rounded-[1000px] bg-[#aeb3af] mb-4">
-              <div class="max-w-[725px] w-full h-[9px] rounded-[1000px] bg-[#00962D] mb-4"></div>
+              <div class="w-full h-[9px] rounded-[1000px] bg-[#aeb3af] mb-4">
+                <div
+                  class="max-w-[725px] w-full h-[9px] rounded-[1000px] bg-[#00962D] mb-4"
+                ></div>
               </div>
             </div>
             <CustomInput
@@ -55,8 +52,6 @@
               type="password"
               class="py-2"
             ></CustomInput>
-
-
           </q-card-section>
 
           <!-- Submit -->
@@ -68,13 +63,12 @@
 
           <!-- Error -->
 
-            <div
-              v-if="error"
-              class="text-[#ED1C24] font-[Arena_Uno] text-[25px] font-normal text-center mt-8"
-            >
-              Your Passwords do not match, please try again
-            </div>
-
+          <div
+            v-if="error"
+            class="text-[#ED1C24] font-[Arena_Uno] text-[25px] font-normal text-center mt-8"
+          >
+            Your Passwords do not match, please try again
+          </div>
         </q-card>
       </q-form>
     </div>
@@ -100,13 +94,13 @@ const handleLogin = () => {
 };
 </script>
 <style>
-.perfectText{
+.perfectText {
   width: 725px;
   color: #003329;
-font-family: "Arena Uno";
-font-size: 20px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
+  font-family: "Arena Uno";
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 }
 </style>
